472a90aa66f931bb4c13e27df9df225170e0d63b
Adnan Ahmad <0codewithadnan0@gmail.com>
2026-06-26 00:44:33 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+
+const App = function () {
+    return (
+        <>
+            <main>
+                <Navbar />
+            </main>
+        </>
+    )
+}
+
+export default App
