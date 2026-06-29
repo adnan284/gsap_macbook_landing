042d1a0e30d089a00a1f0d6b9e6e0d4c5696bcbb
Adnan Ahmad <0codewithadnan0@gmail.com>
2026-06-29 21:41:18 +0500

    GSAP animation for MacBook size transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import ProductViewer from "./components/ProductViewer"
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = function () {
     return (
@@ -7,6 +12,7 @@ const App = function () {
             <main>
                 <Navbar />
                 <Hero />
+                <ProductViewer />
             </main>
         </>
     )
